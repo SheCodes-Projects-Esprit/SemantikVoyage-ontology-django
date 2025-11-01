8204e62ededcb2a13144f5c652608b1a4e89f7ae
FouzriChayma <chayma.fouzri@esprit.tn>
2025-11-01 13:09:47 +0100

    change to template sections

--- core/templatetags/dict_filters.py
+++ core/templatetags/dict_filters.py
@@ -1,7 +1,10 @@
+# core/templatetags/dict_filters.py
 from django import template
 
 register = template.Library()
 
-@register.filter
+@register.filter(name='get_item', is_safe=True)
 def get_item(dictionary, key):
-    return dictionary.get(key, '')
+    if not isinstance(dictionary, dict):
+        return ''
+    return dictionary.get(str(key), '')  # key might be variable name as string
